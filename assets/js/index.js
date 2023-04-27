6116752132a236d40fd89bbb43587dd18411f32c
ZuuZaa <zumbaghirova@gmail.com>
2023-04-28 01:08:58 +0400

    carousel addad

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,33 @@
 const navbar = document.querySelector(".navbar");
+const menuButton = document.querySelector('.menu-button');
+const collapsedNav = document.querySelector('.collapse-wrapper');
+const closeButton = document.querySelector('.cross-button');
+const slider = document.querySelector('.slider-container');
+const sliderButtons = document.querySelectorAll('.slider-btn');
 let scrollTopPosition = 0;
+let sliderIndex = 0;
 
 
+// ---- header scroll behavior ---------
 document.addEventListener("scroll", () => {
     scrollTopPosition = window.scrollY;
     scrollTopPosition > 90 ? navbar.classList.add('onscroll') : navbar.classList.remove('onscroll')
 });
+
+
+// ------ open menu -------- 
+menuButton.addEventListener('click', () => {
+    collapsedNav.classList.add('show-nav');
+})
+
+// ---- close menu --------
+closeButton.addEventListener('click', () => {
+    collapsedNav.classList.remove('show-nav');
+})
+window.addEventListener("resize", () => {
+    collapsedNav.classList.remove('show-nav');
+});
+
+$('.carousel').carousel()
+
+
